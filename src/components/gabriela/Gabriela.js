b90ebdb8f1bf7faf6e87714e9ebe1bc3bf07a524
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-01 17:04:50 -0300

    Update Gabriela.js

--- src/components/gabriela/Gabriela.js
+++ src/components/gabriela/Gabriela.js
@@ -32,50 +32,7 @@ export default function Gabriela() {
           Capítulo 1
         </Button></Link></Card.Body>
     </Card>
-    <Card>
-      <Card.Body><Link to="/Porcategoria"><Button variant="primary" size="lg" width="80%">
-        Filmes Por categoria
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body> <Link to="/Series"><Button variant="primary" size="lg" width="80%">
-        Séries
-        </Button></Link></Card.Body>
-    </Card>
-
-    <Card>
-      <Card.Body> <Link to="/Documentarios"><Button variant="primary" size="lg" width="80%">
-        Documentários
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body> <Link to="/Novelas"><Button variant="primary" size="lg" width="80%">
-        Novelas
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body><Link to="/Playlistyou"><Button variant="primary" size="lg" width="80%">
-        Playlist de filmes do youtube
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body><Link to="/TVs"><Button variant="primary" size="lg" width="80%">
-        TVs
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body><Link to="/Radios"><Button variant="primary" size="lg" width="80%">
-        Radios
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body><Link to="/Youtubeml"><Button variant="primary" size="lg" width="80%">
-        Playlist de Musicas do youtube
-        </Button></Link></Card.Body>
-    </Card>
-    <Card>
-      <Card.Body>O site JPP APP é uma plataforma para assistir filmes e séries online, Esta página funciona com base nas últimas tecnologias lançadas nesta geração voltada ao uso de inteligência artificial, robots e API de dados, ou seja, não hospedamos vídeos em nossos servidores, apenas, de forma automática, indexamos os arquivos que são capturados pela web, nos tornando completamente legais e dentro da lei. O uso desta plataforma é de total responsabilidade do usuário. Qualquer violação de direitos autorais, entre em contato com o distribuidor. Em caso de dúvidas ou reclamações sobre o conteúdo, entre em contato conosco para obter a devida ajuda.</Card.Body>
-    </Card>
+   
           
   
         </nav>
