59e55e66c9cd18a75433e1790f4c48f25d8e7b05
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-05 13:19:47 -0300

    mm

--- src/components/pages/Teste2.js
+++ src/components/pages/Teste2.js
@@ -1,52 +1,59 @@
-import React from "react";
+import React from 'react';
+import Videojs from './video.js';
 import { Link } from 'react-router-dom';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Sbt() {
 
-  return (
-    <div>
-          <p><br /></p>
-          <div><Container fluid>
-            <Row>
-              <Col>
-      <iframe
-        src="https://www.youtube.com/embed/ABVQXgr2LW4?autoplay=true"
-        target="_blank"
-        scrolling="no"
-        data-draggable="none"
-        marginwidth="0"
-        marginheight="0"
-        data-popup="none" 
-        frameborder="0"
-        width="100%" height="600px"
-        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
-        title="video"
-      />{" "}
-      </Col></Row></Container>
 
-                  <div className="nave">
+
+const videoJsOptions = {
+  autoplay: false,
+  playbackRates: [0.5, 1, 1.25, 1.5, 2],
+  width: '720px',
+  controls: true,
+  poster: '',
+  sources: [
+    {
+
+      src: 'https://service-stitcher-ipv4.clusters.pluto.tv/stitch/hls/channel/5f120f41b7d403000783a6d6/master.m3u8?advertisingId=&appName=web&appVersion=unknown&architecture=&buildVersion=&clientTime=&deviceDNT=0&deviceId=d3c67da60ce646b98729283971eb92b1&deviceLat=-1.4537&deviceLon=-48.5078&deviceMake=Chrome&deviceModel=Chrome&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&marketingRegion=BR&sid=91f46f2a10fd11ecbd449a01a2268404&userId=',
+      type: 'application/vnd.apple.mpegurl',
+      
+    },
+  ],
+};
+
+const Teste2 = () =>
+{
+  return (<>
+
+  <Container fluid>
+            <p></p>
+              <div>
+    <Videojs {...videoJsOptions} />  </div>
+    
+    <p></p>
+   </Container>
+ 
+  <div className="nave">
      
                   <p><br /></p>
 
 <Link to="/"><Button variant="primary" size="lg" width="80%">
 Home
   </Button></Link>
-  <div id="player">
-  
-  </div>
+
 <p><br /></p>
 </div>
-</div></div>
+  
+  </>
+  
+  )}
 
-  );
-}
+  export default Teste2
 
 
 
 
 
+  
