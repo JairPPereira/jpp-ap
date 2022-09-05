765fe5e4f7e39844243d97d17a54ebca0be71ea2
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-04 23:37:56 -0300

    Update Gabriela5.js

--- src/components/gabriela/Gabriela5.js
+++ src/components/gabriela/Gabriela5.js
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import ReactPlayer from "react-player";
+import React from "react";
+import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Vid from "https://lb2x01.g1novelas.top/stream/720/ZUAAvtU9lSs62gJ/__001";
 
 export default function Gabriela5() {
-  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
-  const onLoadedData = () => {
-    setIsVideoLoaded(true);
-  };
   return (
-    <div className="Gabriela5">
-      <h1>react-player-mp4</h1>
-      <Container>
-       
-        <div style={{ opacity: isVideoLoaded ? 1 : 0 }}>
-          <ReactPlayer
-            url={Vid}
-            playing={true}
-            controls={true}
-            loop={true}
-            muted={true}
-            playsinline={true}
-            onReady={onLoadedData}
-          />
-        </div>
-      </Container>
-    </div>
+    <div>
+          <p><br /></p>
+          <div><Container fluid>
+            <Row>
+              <Col>
+      <iframe
+        src="https://lb2x01.g1novelas.top/stream/720/ZUAAvtU9lSs62gJ/__001"
+        target="_blank"
+        scrolling="no"
+        data-draggable="none"
+        marginwidth="0"
+        marginheight="0"
+        data-popup="none" 
+        frameborder="0"
+        width="100%" height="600px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+      />{" "}
+      </Col></Row></Container>
+                  <div className="nave">
+     
+                  <p><br /></p>
+
+<Link to="/"><Button variant="primary" size="lg" width="80%">
+Home
+  </Button></Link>
+
+<p><br /></p>
+</div>
+</div></div>
+
   );
 }
 
-const Container = styled.div`
-  width: 100%;
-  position: relative;
-  .video-thumb {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    transition: opacity 400ms ease 0ms;
-  }
-  .tiny {
-    filter: blur(20px);
-    transform: scale(1.1);
-    transition: visibility 0ms ease 400ms;
-  }
-`;
+
+
+
+
