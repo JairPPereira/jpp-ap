c124f74904e7c68b98c332af1165ae5b73024fd6
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-07 17:34:02 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -65,48 +65,48 @@ function Home() {
   
         <Container>
         <Card>
-      <Card.Body><Link to="/Daaz7"><Button variant="primary" size="lg" width="100%">
-          Todos os Filmes
+      <Card.Body><h3>Todos os Filmes</h3><Link to="/Daaz7"><Button variant="primary" size="lg" width="100%">
+          Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body><Link to="/Porcategoria"><Button variant="primary" size="lg" width="80%">
-        Filmes Por categoria
+      <Card.Body><h3>Filmes Por categoria</h3><Link to="/Porcategoria"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body> <Link to="/Series"><Button variant="primary" size="lg" width="80%">
-        Séries
+      <Card.Body><h3>Séries</h3><Link to="/Series"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
 
     <Card>
-      <Card.Body> <Link to="/Documentarios"><Button variant="primary" size="lg" width="80%">
-        Documentários
+      <Card.Body><h3>Documentários</h3><Link to="/Documentarios"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body> <Link to="/Novelas"><Button variant="primary" size="lg" width="80%">
-        Novelas
+      <Card.Body><h3>Novelas</h3><Link to="/Novelas"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body><Link to="/Playlistyou"><Button variant="primary" size="lg" width="80%">
-        Playlist de filmes do youtube
+      <Card.Body><h3>Playlist de filmes do youtube</h3><Link to="/Playlistyou"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body><Link to="/TVs"><Button variant="primary" size="lg" width="80%">
-        TVs
+      <Card.Body><h3>TVs</h3><Link to="/TVs"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body><Link to="/Radios"><Button variant="primary" size="lg" width="80%">
-        Radios
+      <Card.Body><h3>Radios</h3><Link to="/Radios"><Button variant="primary" size="lg" width="80%">
+      Abrir
         </Button></Link></Card.Body>
     </Card>
     <Card>
-      <Card.Body><Link to="/Youtubeml"><Button variant="primary" size="lg" width="80%">Musicas do youtube</Button></Link></Card.Body>
+      <Card.Body><h3>Musicas do youtube</h3><Link to="/Youtubeml"><Button variant="primary" size="lg" width="80%">Abrir</Button></Link></Card.Body>
     </Card>
     <Card>
       <Card.Body>O site JPP APP é uma plataforma para assistir filmes e séries online, Esta página funciona com base nas últimas tecnologias lançadas nesta geração voltada ao uso de inteligência artificial, robots e API de dados, ou seja, não hospedamos vídeos em nossos servidores, apenas, de forma automática, indexamos os arquivos que são capturados pela web, nos tornando completamente legais e dentro da lei. O uso desta plataforma é de total responsabilidade do usuário. Qualquer violação de direitos autorais, entre em contato com o distribuidor. Em caso de dúvidas ou reclamações sobre o conteúdo, entre em contato conosco para obter a devida ajuda.</Card.Body>
