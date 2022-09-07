d1e7ee70f68ca50a4d16526f4375b3215ce28bc1
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-07 17:24:05 -0300

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -50,6 +50,7 @@ function Home() {
           Proxima pagina
         </Button></Link>
 </div></center>
+
 <center>
     
     <div>
@@ -62,7 +63,7 @@ function Home() {
         <nav>
         <p><br /></p>
   
-        
+        <Container>
         <Card>
       <Card.Body><Link to="/Daaz7"><Button variant="primary" size="lg" width="100%">
           Todos os Filmes
@@ -109,7 +110,7 @@ function Home() {
     </Card>
     <Card>
       <Card.Body>O site JPP APP é uma plataforma para assistir filmes e séries online, Esta página funciona com base nas últimas tecnologias lançadas nesta geração voltada ao uso de inteligência artificial, robots e API de dados, ou seja, não hospedamos vídeos em nossos servidores, apenas, de forma automática, indexamos os arquivos que são capturados pela web, nos tornando completamente legais e dentro da lei. O uso desta plataforma é de total responsabilidade do usuário. Qualquer violação de direitos autorais, entre em contato com o distribuidor. Em caso de dúvidas ou reclamações sobre o conteúdo, entre em contato conosco para obter a devida ajuda.</Card.Body>
-    </Card>
+    </Card></Container>
           
   
         </nav>
