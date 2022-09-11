39373f78b598a9ba2dfd527d7e95cfafcf245582
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-10 21:18:49 -0300

    hh

--- src/components/pages/TVs.js
+++ src/components/pages/TVs.js
@@ -43,9 +43,9 @@ export default function TVs() {
     </Card>
 
     <Card>
-    <Card.Body><Link to="./Teste2"><Button variant="primary" size="lg" width="100%">
+    <Card.Body><a href="https://music.youtube.com/watch?v=NWqnYU6Ors0&list=RDAMPLPLQp-UAdzge47nte4vXsnIDBeQjAQSzfyc"><Button variant="primary" size="lg" width="100%">
     Teste 2
-        </Button></Link></Card.Body>
+        </Button></a></Card.Body>
     </Card>
 
 
--- src/components/pages/Teste3.js
+++ src/components/pages/Teste3.js
@@ -1,77 +1,49 @@
-import React, { Component } from 'react';
-import VideoPlayer from 'react-video-js-player';
+import React from "react";
 import { Link } from 'react-router-dom';
-import './videoapp.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
- 
-class Teste3 extends Component {
-    player = {}
-    state = {
-        video: {
-            src: "https://live-lib-pa-01.video.globo.com/d/s/hls-globo-bel/playlist.m3u8?h=0401661638062123011124716615516621291573014QmdmhBJzKGpFrJD-YHB1tw&k=html5&a=F&u=86b5c605-29bd-49ee-94fd-38157902dfa7",
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-            poster: ""
-        }
-    }
- 
-    onPlayerReady(player){
-        console.log("Player is ready: ", player);
-        this.player = player;
-    }
- 
-    onVideoPlay(duration){
-        console.log("Video played at: ", duration);
-    }
- 
-    onVideoPause(duration){
-        console.log("Video paused at: ", duration);
-    }
- 
-    onVideoTimeUpdate(duration){
-        console.log("Time updated: ", duration);
-    }
- 
-    onVideoSeeking(duration){
-        console.log("Video seeking: ", duration);
-    }
- 
-    onVideoSeeked(from, to){
-        console.log(`Video seeked from ${from} to ${to}`);
-    }
- 
-    onVideoEnd(){
-        console.log("Video ended");
-    }
- 
-    render() {
-        return (
-            <div>
-                      <p><br /></p>
-                <VideoPlayer
-                    controls={true}
-                    src={this.state.video.src}
-                    poster={this.state.video.poster}
-                    width="720px" 
-                    height="auto"
-                    onReady={this.onPlayerReady.bind(this)}
-                    onPlay={this.onVideoPlay.bind(this)}
-                    onPause={this.onVideoPause.bind(this)}
-                    onTimeUpdate={this.onVideoTimeUpdate.bind(this)}
-                    onSeeking={this.onVideoSeeking.bind(this)}
-                    onSeeked={this.onVideoSeeked.bind(this)}
-                    onEnd={this.onVideoEnd.bind(this)}
-                />
-                    <p><br /></p> 
-               <div className="nave">
-      <nav>
-      <p><br /></p>
-      <Link to="/">Home</Link>
+export default function Sbt() {
+  return (
+    <div>
+          <p><br /></p>
+          <div><Container fluid>
+            <Row>
+              <Col>
+      <iframe
+        src="https://music.youtube.com/watch?v=3-4WgnZVicU"
+        target="_blank"
+        scrolling="no"
+        data-draggable="none"
+        marginwidth="0"
+        marginheight="0"
+        data-popup="none" 
+        frameborder="0"
+        width="100%" height="600px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+      />{" "}
+      </Col></Row></Container>
+                  <div className="nave">
+     
+                  <p><br /></p>
+
+<Link to="/"><Button variant="primary" size="lg" width="80%">
+Home
+  </Button></Link>
 
 <p><br /></p>
+</div>
+</div></div>
 
-      </nav></div></div>
-            
-        );
-    }
+  );
 }
-export default Teste3;
+
+
+
+
+
