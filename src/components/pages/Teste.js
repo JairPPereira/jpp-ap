42221ef6a43d1665fe692cb127b103704ea7c4f1
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-12 19:50:09 -0300

    Update Teste.js

--- src/components/pages/Teste.js
+++ src/components/pages/Teste.js
@@ -14,7 +14,7 @@ class Teste extends Component {
     player = {}
     state = {
         video: {
-            src: "https://live-lib-pa-01.video.globo.com/d/s/hls-globo-bel/playlist.m3u8?h=0401662940813133531063816628544135410546432IQunntgwmiif4xJhyKxp2g&k=html5&a=F&u=86b5c605-29bd-49ee-94fd-38157902dfa7",
+            src: "https://live-as-02-25.video.globo.com/d/s/hls-globo-bel/playlist.m3u8?h=0401663109240126769917216630228401492497530S76GiK6Vb3DiWg2h_1FzEw&k=html5&a=F&u=86b5c605-29bd-49ee-94fd-38157902dfa7",
             crossorigin: "anonymous",
 
             poster: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/b8/Logotipo_da_Rede_Liberal.png/200px-Logotipo_da_Rede_Liberal.png"
