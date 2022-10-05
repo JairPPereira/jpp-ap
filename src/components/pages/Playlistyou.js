ffaa7a331e74e668adaa6f8b6c4d2471cd145d1a
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-10-05 17:19:25 -0300

    Update Playlistyou.js

--- src/components/pages/Playlistyou.js
+++ src/components/pages/Playlistyou.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../App.css';
 import {Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export default function Playlistyou() {
 
@@ -15,6 +17,17 @@ export default function Playlistyou() {
       <Link to="./Playclass">Playlist de filmes por Roberto Ludueno</Link>
       <p><br /></p>
       <Link to="./Playnetmovie">Playlist de filmes NetMovie</Link>
+      <p><br /></p>
+      
+      <Card.Body>
+        <Card.Text><p2>Playlist 2</p2>
+        </Card.Text>
+        <a href="https://jpp-ap.onrender.com/filmesy/"><Button variant="primary" size="lg" width="100%"><p1>Assistir</p1>
+        
+        </Button></a>
+      </Card.Body>
+
+      <Link to="https://jpp-ap.onrender.com/filmesy/">Playlist de filmes NetMovie</Link>
       <p><br /></p>
       <Link to="./Youtubeml">Playlist de musicas</Link>
       <p><br /></p>
