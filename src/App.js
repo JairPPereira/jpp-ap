e3fefbad891482b026f95c38b7c1d655e76b0ed7
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-11-23 15:58:55 -0300

    gg

--- src/App.js
+++ src/App.js
@@ -420,6 +420,7 @@ import Oculpado from './components/pages2/Oculpado';
 import Alexrct from './components/pages2/Alexrct';
 import Oscacadores from './components/pages2/Oscacadores';
 import Adesencantada from './components/pages2/Adesencantada';
+import Cavaleirodalua from './components/pages2/Cavaleirodalua';
 
 
 
@@ -858,6 +859,7 @@ function App() {
         <Route path='/9978' component={Alexrct} />
         <Route path='/231474' component={Oscacadores} />
         <Route path='/338958' component={Adesencantada} />
+        <Route path='/cavaleirodalua' component={Cavaleirodalua} />
 
        
 
--- src/components/pages/Series.js
+++ src/components/pages/Series.js
@@ -53,6 +53,17 @@ export default function Series() {
         </Button></Link>
       </Card.Body>
     </Card></center>
+    <center>
+    <p></p>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2/tkc7AVyUoG9VEeDvukN0TVqa24C.jpg" />
+      <Card.Body>
+        <Card.Title>Cavaleiro da Lua (2022)</Card.Title>
+        <Link to="./Cavaleirodalua"><Button variant="primary" size="lg" width="100%">
+      Assistir
+        </Button></Link>
+      </Card.Body>
+    </Card></center>
 
 
     <center>
