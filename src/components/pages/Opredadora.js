28fc51a805232998bd04678f19a39ad63a1e24ca
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-12-22 07:42:44 -0300

    Update Opredadora.js

--- src/components/pages/Opredadora.js
+++ src/components/pages/Opredadora.js
@@ -7,7 +7,7 @@ export default function Opredadora() {
     <div>
           <p><br /></p>
       <iframe
-        src="https://streamtape.com/e/VmdM9WWpJouKeYk/"
+        src="https://embedflix.net/filme/tt11866324"
         frameborder="0"
         width="100%" height="600px"
         allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
