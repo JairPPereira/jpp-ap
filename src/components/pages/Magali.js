eb5ae23d099a00f8a9e994ac027d3b0a40175203
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-12-23 22:25:19 -0300

    Update Magali.js

--- src/components/pages/Magali.js
+++ src/components/pages/Magali.js
@@ -8,7 +8,7 @@ import './videoapp.css';
           <div>
                 <p><br /></p>
             <iframe
-              src="ttps://voe.sx/e/y3hiwuwc3yft"
+              src="https://voe.sx/e/y3hiwuwc3yft"
               frameborder="0"
               width="100%" height="480px"
               allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
