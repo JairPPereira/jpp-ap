470544658c0c02bb6fe4875329d43c83b877e0ca
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-01-14 14:48:43 -0300

    Update Amityvilleot.js

--- src/components/pages/Amityvilleot.js
+++ src/components/pages/Amityvilleot.js
@@ -1,58 +1,49 @@
-import React, { Component } from 'react';
-import './videoapp.css';
-import dashjs from 'dashjs';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
-
+import Iframe from 'react-iframe'
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+export default function Amityvilleot() {
+  return (
+    <div>
+          <p><br /></p>
+          <div><Container fluid>
+            <Row>
+              <Col>
+      <Iframe
+        src="https://ok.ru/videoembed/910865795703"
+        target="_blank"
+        scrolling="no"
+        data-draggable="none"
+        marginwidth="0"
+        marginheight="0"
+        data-popup="none" 
+        frameborder="0"
+        width="100%" height="600px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+      />{" "}
+      </Col></Row></Container>
+                  <div className="nave">
+     
+                  <p><br /></p>
 
-
- 
-class Amityvilleot extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-          url: null
-        };
-      }
-    
-      componentDidMount(){
-        let url = "https://vod.provider.plex.tv/library/parts/6298859736001-dash.mpd?X-Plex-Session-Identifier=qwyte7twakn8jnq9scv7mid1&X-Plex-Product=Plex%20Web&X-Plex-Version=4.89.1&X-Plex-Client-Identifier=d0yzzrm42oucmj03766m2cfs&X-Plex-Platform=Chrome&X-Plex-Platform-Version=32.0&X-Plex-Features=external-media%2Cindirect-media%2Chub-style-list&X-Plex-Model=hosted&X-Plex-Device=Android&X-Plex-Device-Name=Chrome&X-Plex-Device-Screen-Resolution=980x1780%2C320x712&X-Plex-Token=EKXrK4UGPTk2os736AYM&X-Plex-Language=pt-BR&Accept-Language=pt-BR";
-        let player = dashjs.MediaPlayer().create();
-        player.initialize(document.querySelector("#videoPlayer"), url, true);
-      }
-    
-      render() {
-        return (
-          <div>
-            <Container fluid>
-      <Row><p><br /></p>
-        <Col><video id="videoPlayer" controls width="100%" height="auto"></video>
-        </Col>
-      </Row>
-    </Container><center>
-      
-    <nav>
-      <p><br /></p>
- 
-      <Link to="/"><Button variant="primary" size="lg" width="80%">
-      Home
-        </Button></Link>
+<Link to="/"><Button variant="primary" size="lg" width="80%">
+Home
+  </Button></Link>
 
 <p><br /></p>
+</div>
+</div></div>
 
-      </nav></center>
-          </div>
-        );
-      }
-    
-
+  );
 }
 
 
-export default Amityvilleot;
+
+
+
