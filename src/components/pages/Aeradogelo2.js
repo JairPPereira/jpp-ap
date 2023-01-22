d9928671e150e4e87d49b29266c3a7213ae2efbb
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-01-22 10:36:32 -0300

    Update Aeradogelo2.js

--- src/components/pages/Aeradogelo2.js
+++ src/components/pages/Aeradogelo2.js
@@ -1,35 +1,49 @@
-import React, { Component } from 'react';
-import './videoapp.css';
-import dashjs from 'dashjs';
+import React from "react";
+import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Iframe from 'react-iframe'
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+export default function Aeradogelo2() {
+  return (
+    <div>
+          <p><br /></p>
+          <div><Container fluid>
+            <Row>
+              <Col>
+      <Iframe
+        src="https://embedflix.net/filme/tt0438097"
+        target="_blank"
+        scrolling="no"
+        data-draggable="none"
+        marginwidth="0"
+        marginheight="0"
+        data-popup="none" 
+        frameborder="0"
+        width="100%" height="600px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+      />{" "}
+      </Col></Row></Container>
+                  <div className="nave">
+     
+                  <p><br /></p>
 
+<Link to="/"><Button variant="primary" size="lg" width="80%">
+Home
+  </Button></Link>
 
- 
-class Aeradogelo2 extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-          url: null
-        };
-      }
-    
-      componentDidMount(){
-        let url = "https://vod.provider.plex.tv/library/parts/6308468981112-dash.mpd?X-Plex-Session-Identifier=nxe9bsfvnbgauzk9kacgy9h1&X-Plex-Product=Plex%20Web&X-Plex-Version=4.89.1&X-Plex-Client-Identifier=d0yzzrm42oucmj03766m2cfs&X-Plex-Platform=Chrome&X-Plex-Platform-Version=104.0&X-Plex-Features=external-media%2Cindirect-media%2Chub-style-list&X-Plex-Model=hosted&X-Plex-Device=Android&X-Plex-Device-Name=Chrome&X-Plex-Device-Screen-Resolution=980x1780%2C320x712&X-Plex-Token=EKXrK4UGPTk2os736AYM&X-Plex-Language=pt-BR&Accept-Language=pt-BR";
-        let player = dashjs.MediaPlayer().create();
-        player.initialize(document.querySelector("#videoPlayer"), url, true);
-      }
-    
-      render() {
-        return (
-          <div>
-            <video id="videoPlayer" controls></video>
-          </div>
-        );
-      }
-    
+<p><br /></p>
+</div>
+</div></div>
 
+  );
 }
 
 
-export default Aeradogelo2;
+
+
+
