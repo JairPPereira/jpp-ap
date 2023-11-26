121fb0df54ac7e18fb7aee25516367f3408a9f92
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-11-26 17:11:23 -0300

    ggg

--- src/components/pages3/Daaz22.js
+++ src/components/pages3/Daaz22.js
@@ -42,7 +42,7 @@ function Daaz22() {
           Anterior
         </Button></Link>
  
-  <Link to="/daaz"><Button variant="primary" size="lg" width="80%">
+  <Link to="/daaz23"><Button variant="primary" size="lg" width="80%">
           Proxima
         </Button></Link>
 </div></center>
